ee34d6ee532b458c547812273b5a837737f92217
wenjiangit <824636515@qq.com>
2017-02-11 13:24:41 +0800

    查询功能

---
+++ app/src/main/java/com/wenjian/coolweather/ChooseAreaFragment.java
@@ -0,0 +1,278 @@
+package com.wenjian.coolweather;
+
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.wenjian.coolweather.db.City;
+import com.wenjian.coolweather.db.County;
+import com.wenjian.coolweather.db.Province;
+import com.wenjian.coolweather.util.HttpUtil;
+import com.wenjian.coolweather.util.Utility;
+
+import org.litepal.crud.DataSupport;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ChooseAreaFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ChooseAreaFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+    private static final int LEVLE_PROVINCE = 0;
+    private static final int LEVLE_CITY = 1;
+    private static final int LEVLE_COUNTY = 2;
+
+    private static final String BASE_API = "http://guolin.tech/api/china";
+    
+    
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+    private TextView mTitleText;
+    private Button mBackButton;
+    private ListView mListView;
+    private List<String> dataList = new ArrayList<>();
+    private ArrayAdapter<String> mAdapter;
+    
+    private List<Province> mProvinceList;
+    private List<City> mCityList;
+    private List<County> mCountyList;
+    private Province selectedProvince;
+    private City selectedCity;
+    private int currentLevel;
+    private ProgressDialog mProgressDialog;
+
+
+    public ChooseAreaFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment ChooseAreaFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static ChooseAreaFragment newInstance(String param1, String param2) {
+        ChooseAreaFragment fragment = new ChooseAreaFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_choose_area, container, false);
+        mTitleText = (TextView) view.findViewById(R.id.title_text);
+        mBackButton = (Button) view.findViewById(R.id.back_button);
+        mListView = (ListView) view.findViewById(R.id.list_view);
+        mAdapter = new ArrayAdapter<>(getContext(),android.R.layout.simple_list_item_1,dataList);
+        mListView.setAdapter(mAdapter);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (currentLevel == LEVLE_PROVINCE){
+                    selectedProvince = mProvinceList.get(position);
+                    queryCities();
+                } else if (currentLevel == LEVLE_CITY) {
+                    selectedCity = mCityList.get(position);
+                    queryCounties();
+                }
+            }
+        });
+        
+        mBackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (currentLevel == LEVLE_COUNTY) {
+                    queryCities();
+                } else if (currentLevel == LEVLE_CITY) {
+                    queryProvinces();
+                }
+            }
+        });
+
+        queryProvinces();
+    }
+
+    /**
+     * 查询全国所有的省,优先从数据库查询,如果没有就去服务器上查询
+     */
+    private void queryProvinces() {
+        mTitleText.setText("中国");
+        mBackButton.setVisibility(View.GONE);
+        mProvinceList = DataSupport.findAll(Province.class);
+        if (mProvinceList.size() > 0){
+            dataList.clear();
+            for (Province province:mProvinceList) {
+                dataList.add(province.getProvinceName());
+            }
+            mAdapter.notifyDataSetChanged();
+            mListView.setSelection(0);
+            currentLevel = LEVLE_PROVINCE;
+        }else {
+            String address = BASE_API;
+            queryFromServer(address, "province");
+        }
+    }
+
+    /**
+     * 根据传入的地址和类型从服务器上查询省市县数据
+     * @param address
+     * @param type
+     */
+    private void queryFromServer(String address, final String type) {
+        showProgressDialog();
+        HttpUtil.sendOkHttpRequest(address, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        closeProgressDialog();
+                        Toast.makeText(getContext(), "加载失败", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String responseText = response.body().string();
+                boolean result = false;
+                if ("province".equals(type)) {
+                    result = Utility.handleProvinceResponce(responseText);
+                } else if ("city".equals(type)) {
+                    result = Utility.handleCityResponse(responseText,selectedProvince.getId());
+                } else if ("county".equals(type)) {
+                    result = Utility.handleCountyResponse(responseText, selectedCity.getId());
+                }
+
+                if (result) {
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            closeProgressDialog();
+                            if ("province".equals(type)) {
+                                queryProvinces();
+                            } else if ("city".equals(type)) {
+                                queryCities();
+                            } else if ("county".equals(type)) {
+                                queryCounties();
+                            }
+                        }
+                    });
+                }
+
+            }
+        });
+    }
+
+    private void closeProgressDialog() {
+        if (mProgressDialog != null) {
+            mProgressDialog.dismiss();
+        }
+    }
+
+    private void showProgressDialog() {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(getActivity());
+            mProgressDialog.setMessage("正在加载...");
+            mProgressDialog.setCanceledOnTouchOutside(false);
+        }
+        mProgressDialog.show();
+    }
+
+    /**
+     * 查询选中的市所有的县，优先从数据库查询，如果没有就去服务器上查询
+     */
+    private void queryCounties() {
+        mTitleText.setText(selectedCity.getCityName());
+        mBackButton.setVisibility(View.VISIBLE);
+        mCountyList = DataSupport.where("cityid = ?", String.valueOf(selectedCity.getId())).find(County.class);
+        if (mCountyList.size() > 0) {
+            dataList.clear();
+            for (County county : mCountyList) {
+                dataList.add(county.getCountyName());
+            }
+            mAdapter.notifyDataSetChanged();
+            mListView.setSelection(0);
+            currentLevel = LEVLE_COUNTY;
+        }else {
+            int cityCode = selectedCity.getCityCode();
+            int provinceCode = selectedProvince.getProvinceCode();
+            String address = BASE_API + "/" + provinceCode + "/" + cityCode;
+            queryFromServer(address,"county");
+        }
+    }
+
+    /**
+     * 查询选中省内的所有的市,优先从数据库查询,如果没有查询到再去服务器上查询
+     */
+    private void queryCities() {
+        mTitleText.setText(selectedProvince.getProvinceName());
+        mBackButton.setVisibility(View.VISIBLE);
+        mCityList = DataSupport.where("provinceid = ?",
+                String.valueOf(selectedProvince.getId())).find(City.class);
+        if (mCityList.size() > 0) {
+            dataList.clear();
+            for (City city : mCityList) {
+                dataList.add(city.getCityName());
+            }
+            mAdapter.notifyDataSetChanged();
+            mListView.setSelection(0);
+            currentLevel = LEVLE_CITY;
+        }else {
+            int provinceCode = selectedProvince.getProvinceCode();
+            String address = BASE_API + File.separator+provinceCode;
+            queryFromServer(address,"city");
+        }
+    }
+}
+
